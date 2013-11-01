e3b7f59a7fda0278ca890590043f603b630f2d75
Matthew Bunday <zenceph@gmail.com>
2013-11-01 15:03:12 -0400

    Update existing prose.

--- prosedy-mt.js
+++ prosedy-mt.js
@@ -16,11 +16,24 @@ if (Meteor.isClient) {
     return {title: "New Prose", text: "Start your new piece of prose here..."}
   }
 
+  Template.prose.live_prose = function() {
+    var title = document.getElementById("prose_title").value;
+    var text = document.getElementById("prose_text").value;
+    return {title: title, text: text};
+  }
+
   Template.prose.events({
     'click input.save_new': function() {
-      var title = document.getElementById("prose_title").value;
-      var text = document.getElementById("prose_text").value;
-      Proses.insert({title: title, text: text, live: true});
+      var prose = Template.prose.live_prose();
+      prose['live'] = true;
+      Proses.insert(prose);
+    },
+    'click input.save_existing': function() {
+      var prose = Template.prose.prose();
+      var live_prose = Template.prose.live_prose();
+      prose['title'] = live_prose['title'];
+      prose['text'] = live_prose['text'];
+      Proses.update(prose['_id'], prose);
     }
   })
 
