e7800743cbb88b5a4c63a9f7bdc4976f2c3ec505
Matthew Bunday <zenceph@gmail.com>
2013-11-13 04:40:50 -0500

    Messing with this cross domain login.

---
+++ headers-common.js
@@ -0,0 +1,10 @@
+headers = {
+        list: {},
+        proxyCount: 0,
+        setProxyCount: function(proxyCount) {
+                this.proxyCount = proxyCount;
+        },
+        getProxyCount: function() {
+                return this.proxyCount;
+        }
+}
