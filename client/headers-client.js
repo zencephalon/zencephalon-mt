2f09f716562c51ca0a363983fa160933a8f49d45
Matthew Bunday <zenceph@gmail.com>
2013-11-13 05:01:12 -0500

    Messing with this cross domain login.

--- client/headers-client.js
+++ client/headers-client.js
@@ -57,19 +57,3 @@ window.addEventListener("message", function(event) {
   }
 });
 
-Meteor.call('getRootUrl', function(error, getRootUrl) {
-  rootUrl = getRootUrl;
-  var rootHost = /https?:\/\/([A-Za-z\._-]*).*?/.exec(rootUrl)[1];
-  var httpHost = headers.get('host').split(':')[0];
-  if (rootHost != httpHost) {
-    console.log('Not on ROOT_URL, retrieving login details from there...');
-    var iframe = $('<iframe>')
-      .css('display', 'none')
-      .attr('src', rootUrl+'fetchLoginId');
-    iframe.load(function() {
-      console.log('loaded');
-      this.contentWindow.postMessage("x", rootUrl);
-    });
-    $('body').append(iframe);
-  }
-});  
