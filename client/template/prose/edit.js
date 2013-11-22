5a8e6843724a508ecae01aa60dc56101bea041bf
Matthew Bunday <zenceph@gmail.com>
2013-11-21 23:49:15 -0500

    Removing unnecessary client wrapping

--- client/template/prose/edit.js
+++ client/template/prose/edit.js
@@ -1,43 +1,40 @@
-if (Meteor.isClient) {
-  Template.prose_edit.live_prose = function() {
-    var o = {};
-    ["title", "text", "url"].forEach(function(ele) {
-      o[ele] = $("#prose_" + ele).val();
-    });
-    return o;
-  }
-
-  Template.prose_edit.prose = function() {
-    return Session.get("selected_prose");
-  }
-
-  Template.prose_edit.settings = function() {
-    return {
-      position: "bottom",
-      limit: 5,
-      rules: [
-      {
-        token: '](',
-        collection: Proses,
-        field: "url",
-        template: Template.prose_url
-      }]
-    }
-  }
-
-  Template.prose_edit.events({
-    'click input.save': function() {
-      live_prose = Template.prose_edit.live_prose();
-      prose = Template.prose_edit.prose();
-      branch = Session.get("selected_branch");
-
-      Prose.save(prose, live_prose, branch);
-
-      Session.set("view_mode", true);
-      Router.go('prose', {url: live_prose["url"]});
-    },
-    'click a.edit_toggle': function() {
-      Session.set("view_mode", true);
-    }
+Template.prose_edit.live_prose = function() {
+  var o = {};
+  ["title", "text", "url"].forEach(function(ele) {
+    o[ele] = $("#prose_" + ele).val();
   });
+  return o;
 }
+
+Template.prose_edit.prose = function() {
+  return Session.get("selected_prose");
+}
+
+Template.prose_edit.settings = function() {
+  return {
+    position: "bottom",
+    limit: 5,
+    rules: [{
+      token: '](',
+      collection: Proses,
+      field: "url",
+      template: Template.prose_url
+    }]
+  }
+}
+
+Template.prose_edit.events({
+  'click input.save': function() {
+    live_prose = Template.prose_edit.live_prose();
+    prose = Template.prose_edit.prose();
+    branch = Session.get("selected_branch");
+
+    Prose.save(prose, live_prose, branch);
+
+    Session.set("view_mode", true);
+    Router.go('prose', {url: live_prose["url"]});
+  },
+  'click a.edit_toggle': function() {
+    Session.set("view_mode", true);
+  }
+});
