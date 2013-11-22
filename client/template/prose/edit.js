3081c3de1e515140395eca6ba1c238c4ec973ed4
Matthew Bunday <zenceph@gmail.com>
2013-11-22 01:15:30 -0500

    Added keyboard shortcut!

--- client/template/prose/edit.js
+++ client/template/prose/edit.js
@@ -23,18 +23,22 @@ Template.prose_edit.settings = function() {
   }
 }
 
+Template.prose_edit.save_prose = function() {
+  live_prose = Template.prose_edit.live_prose();
+  prose = Template.prose_edit.prose();
+  branch = Session.get("selected_branch");
+
+  Prose.save(prose, live_prose, branch);
+
+  Session.set("view_mode", true);
+  Router.go('prose', {url: live_prose["url"]});
+}
+
 Template.prose_edit.events({
-  'click input.save': function() {
-    live_prose = Template.prose_edit.live_prose();
-    prose = Template.prose_edit.prose();
-    branch = Session.get("selected_branch");
-
-    Prose.save(prose, live_prose, branch);
-
-    Session.set("view_mode", true);
-    Router.go('prose', {url: live_prose["url"]});
-  },
+  'click input.save': Template.prose_edit.save_prose,
   'click a.edit_toggle': function() {
     Session.set("view_mode", true);
   }
 });
+
+Mousetrap.bind('ctrl+s', function(e) { Template.prose_edit.save_prose();});
