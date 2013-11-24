4132af6f5eb93af429283331a7a44ee6ff40c8a2
Matthew Bunday <zenceph@gmail.com>
2013-11-24 03:51:05 -0500

    100% more insane branch display. :D

--- client/template/branch/list.js
+++ client/template/branch/list.js
@@ -6,9 +6,37 @@ if (Meteor.isClient) {
     }
   };
 
-  Template.branch_list.branches = function() {
-    if (Session.get("selected_prose") !== undefined) {
-      return Branches.find({prose: Session.get("selected_prose")._id});
+  Template.branch_list.branch_display = function(current_branch, parent_branch_length) {
+    prose = Session.get("selected_prose");
+    branch_names = _.pluck(Branches.find({prose: prose._id}).fetch(), 'name');
+    output = "<table><tr>";
+    function branch_url(branch, parent_branch_length) {
+      return "<a href='/" + prose.url + "/b/" + branch + "'>" + branch.substr(parent_branch_length) + "</a>";
     }
-  };
+    while (_.contains(branch_names, current_branch)) {
+      output += "<td>";
+
+      if (_.contains(branch_names, current_branch + 'a0')) {
+        output += "<table><tr><td>";
+        output += branch_url(current_branch, parent_branch_length);
+        output += "</td></tr>";
+
+        next_lat_code = 'a';
+        while (_.contains(branch_names, current_branch + next_lat_code + '0')) {
+          output += "<tr><td>";
+          output += Template.branch_list.branch_display(current_branch + next_lat_code + '0', parent_branch_length + 2);
+          output += "</td></tr>";
+          next_lat_code = Branch.next_lateral_code(next_lat_code);
+        }
+        output += "</table>";
+      } else {
+        output += branch_url(current_branch, parent_branch_length);
+      }
+
+      output += "</td>";
+      current_branch = Branch.next_sequential_branch(current_branch);
+    }
+    output += "</tr></table>";
+    return output;
+  }
 }
