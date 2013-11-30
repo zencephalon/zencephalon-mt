622929841fbb97b849fdccc9244503180ac7d2e7
Matthew Bunday <zenceph@gmail.com>
2013-11-30 17:21:31 -0500

    Fix date bug. Change default journal format.

--- model/journals.js
+++ model/journals.js
@@ -1,10 +1,10 @@
 Journal = {
   template : function() {
-    return "- Code?\n- Meditate?\n- Exercise?\n- Taijutsu?\n- Journal?\n"
+    return "#### Code?\n\n#### Meditate?\n\n#### Exercise?\n\n#### Taijutsu?\n\n#### Journal?\n"
   },
   today : function() {
     d = new Date();
-    day_string = d.getMonth() + "_" + d.getDay() + "_" + d.getFullYear();
+    day_string = (d.getMonth() + 1) + "_" + d.getDate() + "_" + d.getFullYear();
     if (Prose.get(day_string)._id === undefined) {
       Prose.create(day_string, day_string, Journal.template(), true);
     } 
