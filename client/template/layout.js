874bfea01d04eabcf1c86784288115d4ee503ab2
Matthew Bunday <zenceph@gmail.com>
2013-12-02 01:56:08 -0500

    Use flowtype.

---
+++ client/template/layout.js
@@ -0,0 +1,10 @@
+Template.layout.rendered = function() {
+  $('body').flowtype({
+    minimum: 0,
+    maximum: 1200,
+    minFont: 12,
+    maxFont: 32,
+    fontRatio: 30,
+    lineRatio: 1.45
+  });
+}
