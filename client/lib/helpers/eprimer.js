44612fbd5807b414ff887a4fc57986e77abde65c
Matthew Bunday <zenceph@gmail.com>
2013-12-26 08:36:11 -0500

    Factored out e-prime helper, made it only active for logged in users.

---
+++ client/lib/helpers/eprimer.js
@@ -0,0 +1,14 @@
+EPrimer = {
+  showErrors : function(text) {
+    _.each(["be", "being", "been", "am", "is", "isn't", "are", "aren't", "was", "wasn't", "were", "weren't", "I'm", "you're", "we're", "they're", "he's", "she's", "it's", "there's", "here's", "where's", "how's", "what's", "who's", "that's"], 
+      function(word) {
+        text = EPrimer.highlighter(word, text);
+    });
+    return text;
+  },
+  highlighter : function(word, input) {
+    var rgxp = new RegExp("\\b" + word + "\\b", 'ig');
+    var repl = '<span class="standout">' + word + '</span>';
+    return input.replace(rgxp, repl);
+  }
+}
--- client/template/prose/view.js
+++ client/template/prose/view.js
@@ -7,17 +7,11 @@ Template.prose_view.events({
 });
 
 Template.prose_view.branch_text = function() {
-  function hiliter(word, input) {
-    var rgxp = new RegExp("\\b" + word + "\\b", 'ig');
-    var repl = '<span class="standout">' + word + '</span>';
-    return input.replace(rgxp, repl);
-  };
   branch = Session.get("selected_branch");
   if (branch !== undefined) {
-    _.each(["be", "being", "been", "am", "is", "isn't", "are", "aren't", "was", "wasn't", "were", "weren't", "I'm", "you're", "we're", "they're", "he's", "she's", "it's", "there's", "here's", "where's", "how's", "what's", "who's", "that's"], 
-      function(word) {
-        branch.text = hiliter(word, branch.text);
-    });
+    if (Meteor.user()) {
+      branch.text = EPrimer.showErrors(branch.text);
+    }
     return branch.text;
   } else {
     return "Loading...";
