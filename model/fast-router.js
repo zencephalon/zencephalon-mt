f22f79f9308bf26837c777cbae5ce64ca008fcb0
Matthew Bunday <zenceph@gmail.com>
2014-02-11 16:12:04 -0500

    Remove unused fast-router file.

--- model/fast-router.js
+++
@@ -1,8 +0,0 @@
-// FastRender.route('/z/prose', function(params) {
-//   this.find(Proses, {journal: {"$ne": true}}, {sort: {updated: -1}});
-//   this.completeSubscriptions(['proses']);
-// })
-
-// FastRender.route('/:url', function(params) {
-  
-// })
