a8e53c59b46751a3fea81d3ab7bfbbb46d62152b
Matthew Bunday <zenceph@gmail.com>
2014-07-09 10:32:56 -0400

    Moved two methods into ProseObj class.

--- model/proses.js
+++ model/proses.js
@@ -18,6 +18,27 @@ ProseObj = function (pojo) {
   }
   this.change_url = func
 }
+
+ProseObj.prototype.change_url = function(url) {
+  if (this.url != url) {
+    Proses.update(this._id, {"$set": {url: url}});
+      Util.bulkUpdate(Branches, {prose: this._id}, {"$set": {url: url}});
+    }
+}
+
+ProseObj.prototype.update = function(title, url, text, branch, new_branch) {
+    url = Util.cleanURL(url);
+    if (new_branch) {
+      new_branch_name = Branch.save(branch.name, this._id, this.tree, text, url, true);
+      this.tree.push(new_branch_name);
+      Proses.update(this._id, {"$set": {branch: new_branch_name, tree: this.tree, url: url, updated: new Date()}});
+    } else {
+      Branch.update(branch._id, this._id, text);
+      Proses.update(this._id, {"$set": {url: url, updated: new Date()}});
+    }
+    this.change_url(url);
+}
+
 Prose = {
   get : function(url) {
     var prose = Proses.findOne({url: url});
@@ -33,25 +54,6 @@ Prose = {
     url = Util.cleanURL(url);
     prose = Proses.insert({title: title, url: url, branch: branch, tree: tree, journal: journal, updated: new Date()})
     Branch.create(prose, url, text, '0', true);
-  },
-  change_url : function(prose, url) {
-    if (prose.url != url) {
-      Proses.update(prose._id, {"$set": {url: url}});
-      //Branches.update({prose: prose._id}, {"$set": {title: title}});
-      Util.bulkUpdate(Branches, {prose: prose._id}, {"$set": {url: url}});
-    }
-  },
-  update : function(prose, title, url, text, branch, new_branch) {
-    url = Util.cleanURL(url);
-    if (new_branch) {
-      new_branch_name = Branch.save(branch.name, prose._id, prose.tree, text, url, true);
-      prose.tree.push(new_branch_name);
-      Proses.update(prose._id, {"$set": {branch: new_branch_name, tree: prose.tree, url: url, updated: new Date()}});
-    } else {
-      Branch.update(branch._id, prose._id, text);
-      Proses.update(prose._id, {"$set": {url: url, updated: new Date()}});
-    }
-    this.change_url(prose, url);
   },
   save : function(prose, live_prose, branch, new_branch) {
     if (prose._id !== undefined) {
