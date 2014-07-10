a7387e1081989f61fea06b27fbc5b31c8a4a2ba5
Matthew Bunday <zenceph@gmail.com>
2014-07-10 12:14:09 -0400

    Moved doCounts into Counts object.

--- model/counts.js
+++ model/counts.js
@@ -6,5 +6,15 @@ Counts = {
   },
   _publication_single : function() {
     return _Counts.find({},{sort: {time: -1}, limit: 1});
+  },
+  doCount : function() {
+    var words = 0;
+    var proses = 0;
+    Proses.find().forEach(function (prose) {
+      var branch = Branch.get(prose._id, prose.branch);
+        words += branch.text.split(' ').length;
+        proses++;
+    });
+    _Counts.insert({prose_count: proses, word_count: words, time: new Date()});
   }
 }
--- server/startup.js
+++ server/startup.js
@@ -4,36 +4,18 @@ if (Meteor.isServer) {
     //Branches.remove({});
     //Proses.update({}, {"$set":{"updated": new Date()}});
     //Branches.update({}, {"$set":{"updated": new Date()}});
-    //Counts.remove({});
+    _Counts.remove({});
     if (Proses.find().count() === 0) {
       Prose.create("My Brain on Zen", "index", "I'm Matthew Bunday and I love you.", false);
       Prose.create("I love Daria!", "i_love_daria", "Daria is seriously the best.", false);
     }
 
-    var doCount = function() {
-      var words = 0;
-      var proses = 0;
-      Proses.find().forEach(function (prose) {
-        var branch = Branch.get(prose._id, prose.branch);
-        //Branches.update(branch._id, {"$set": {active: true} }, {multi:true});
-        words += branch.text.split(' ').length;
-        proses++;
-      });
-      // Branches.find().forEach(function (branch) {
-      //   if (! branch.updated) {
-      //     Branches.update(branch._id, {"$set": {"updated": new Date()}});
-      //   }
-      // });
-      //Branches.update({updated: {"$exists": false}}, {"$set":{"updated": new Date()}})
-      _Counts.insert({prose_count: proses, word_count: words, time: new Date()});
-    }
-
     if (_Counts.find().count() === 0) {
-      doCount();
+      Counts.doCount();
     }
 
     Meteor.setInterval(function() {
-      doCount();
+      Counts.doCount();
     }, 1000*60*60*8); // every eight hours
 
     Meteor.publish("proses", function() { return Proses.find(); });
