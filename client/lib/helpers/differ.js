8d71636a9364b6e6bf24200b06c89c8500dd5dc5
Matthew Bunday <zenceph@gmail.com>
2014-07-17 23:59:25 -0400

    Update differ to use new getBranch method.

--- client/lib/helpers/differ.js
+++ client/lib/helpers/differ.js
@@ -1,8 +1,8 @@
 Differ = {
   diff : function(prose, branch_name, diff_branch_name) {
     if (prose._id !== undefined) {
-      branch = Branches.get(prose._id, branch_name);
-      branch_diff = Branches.get(prose._id, diff_branch_name);
+      branch = prose.getBranch(branch_name);
+      branch_diff = prose.getBranch(diff_branch_name);
       dmp = new diff_match_patch();
       d = dmp.diff_main(branch.text, branch_diff.text);
       dmp.diff_cleanupSemantic(d);
