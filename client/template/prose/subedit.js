d05069103937378992674f2cc661beb9398991e1
Matthew Bunday <zenceph@gmail.com>
2014-07-20 13:48:00 -0400

    Autosave working.

---
+++ client/template/prose/subedit.js
@@ -0,0 +1,13 @@
+Template.prose_subedit.events({
+  'keyup': function(e) {
+    var target = $(e.target);
+
+    new_revision = false;
+    if (branch !== undefined && branch.updated.getTime() + 60 * 1000 * 10 < new Date().getTime()) {
+      new_revision = true;
+    }
+    
+    prose = new Prose(prose);
+    prose.save({title: prose.title, text: target.val(), url: prose.url}, branch, new_revision);
+  }
+});
