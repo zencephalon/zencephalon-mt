e30f35ecbade35e87b4a2c89800f8def3facba2a
Matthew Bunday <zenceph@gmail.com>
2014-07-22 11:31:21 -0400

    Autosave after 700ms of no typing.

--- client/template/prose/subedit.js
+++ client/template/prose/subedit.js
@@ -4,12 +4,21 @@ Template.prose_subedit.events({
     var branch = this.branch;
     var prose = this.prose;
 
-    new_revision = false;
-    if (branch !== undefined && branch.updated.getTime() + 60 * 1000 * 10 < new Date().getTime()) {
-      new_revision = true;
+    var timer = Session.get("autosave_timer");
+
+    if (timer !== undefined) {
+      clearTimeout(timer);
     }
-    
-    prose.save({title: prose.title, text: target.val(), url: prose.url}, branch, new_revision);
+
+    Session.set("autosave_timer", setTimeout(function() {
+      console.log("Saved");
+      new_revision = false;
+      if (branch !== undefined && branch.updated.getTime() + 60 * 1000 * 10 < new Date().getTime()) {
+        new_revision = true;
+      }
+      
+      prose.save({title: prose.title, text: target.val(), url: prose.url}, branch, new_revision);
+    }, 700));
   },
   'select, mouseup': function(e) {
     var target = $(e.target);
