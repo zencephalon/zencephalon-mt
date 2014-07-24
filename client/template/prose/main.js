808f6c189bb2e33b19b1605bb30c4743ef1b3120
Matthew Bunday <zenceph@gmail.com>
2014-07-24 16:49:15 -0400

    Remove broken editor.saveProse for now. Need to refactor and add it back most likely.

--- client/template/prose/main.js
+++ client/template/prose/main.js
@@ -6,7 +6,8 @@ Template.prose.rendered = function() {
   $(document).ready(function() {
     Mousetrap.bind('mod+s', function(e) { 
       if (View.editMode()) { 
-        Editor.saveProse(e.target, false, true);
+        //Editor.saveProse(e.target, false, true);
+        Session.set("view_mode", true);
       } else { 
         Session.set("view_mode", false);
       }
