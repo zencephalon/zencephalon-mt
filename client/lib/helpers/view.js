2c25e6cc62c2da8bf1da25a5afa881bf61f8c182
Matthew Bunday <zenceph@gmail.com>
2014-07-29 23:05:48 -0400

    Cleaning up View module.

--- client/lib/helpers/view.js
+++ client/lib/helpers/view.js
@@ -1,21 +1,22 @@
 View = {
   proseArea : function() { return document.getElementById("prose_text")},
   autosize : function() {
-    setTimeout(function(){$("textarea").trigger('autosize.resize')}, 700);
+    setTimeout(function(){$("textarea").trigger('autosize.resize')}, 400);
   },
   save : function(prose_area) {
     prose_area_jq = $(prose_area);
     if (true) {
       view_id = prose_area_jq.attr("data-prose");
       old_view = Session.get("saved_view");
-      view = {caret_pos: Caret.get(prose_area), scroll_pos: prose_area.scrollTop, window_pos: document.body.scrollTop};
+      
+      view = {caret_pos: Caret.get(prose_area), 
+             scroll_pos: prose_area.scrollTop, 
+             window_pos: document.body.scrollTop};
 
-      if (old_view !== undefined) {
-        old_view[view_id] = view;
-      } else {
+      if (old_view === undefined) {
         old_view = {};
-        old_view[view_id] = view;
       }
+      old_view[view_id] = view;
 
       Session.set("saved_view", old_view);
       Session.set("current_view", view_id);
@@ -25,25 +26,20 @@ View = {
     setTimeout(function() {View.restore(view_id)}, 50);
   },
   restore : function(view_id) {
-    if (true) {
-      if (view_id === undefined) {
-        view_id = Session.get("current_view");
-      }
-      if (view_id !== undefined) {
-        prose_area = $("[data-prose='" + view_id + "']").get(0);
-        if (prose_area !== undefined) {
-          prose_area.focus();
-          view = Session.get("saved_view");
+    if (view_id === undefined) {
+      view_id = Session.get("current_view");
+    }
 
-          if (view !== undefined && (view = view[view_id]) !== undefined) {
-            Caret.set(prose_area, view.caret_pos);
-            prose_area.scrollTop = view.scroll_pos;
-            document.body.scrollTop = document.documentElement.scrollTop = view.window_pos;
-          }
-        }
+    prose_area = $("[data-prose='" + view_id + "']").get(0);
+    if (prose_area !== undefined) {
+      prose_area.focus();
+      view = Session.get("saved_view");
+
+      if (view !== undefined && (view = view[view_id]) !== undefined) {
+        Caret.set(prose_area, view.caret_pos);
+        prose_area.scrollTop = view.scroll_pos;
+        document.body.scrollTop = document.documentElement.scrollTop = view.window_pos;
       }
-    } else {
-      //document.body.scrollTop = document.documentElement.scrollTop = 0;
     }
   },
   getCaret : function(prose_area) {
