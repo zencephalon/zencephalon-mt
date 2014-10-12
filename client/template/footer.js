7e18c29aa51fee0c6657e3858a67d65a71de054f
Matthew Bunday <zenceph@gmail.com>
2014-10-11 21:11:10 -0400

    Refactoring.

--- client/template/footer.js
+++ client/template/footer.js
@@ -1,22 +1,18 @@
 Template.footer.word_count = function() {
-  var count = Counts.get();
-  if (count !== undefined) {
+  if (count = Counts.get()) {
     return count.word_count;
   }
 }
 
 Template.footer.prose_count = function() {
-  var count = Counts.get();
-  if (count !== undefined) {
+  if (count = Counts.get()) {
     return count.prose_count;
   }
 }
 
 Template.footer.content = function() {
   template = Prose.get("__footer_template__");
-  if (template._id !== undefined) {
-    if (template.getBranch() !== undefined) {
-      return template.getBranch().text;
-    }
+  if (template._id && template.getBranch()) {
+    return template.getBranch().text;
   }
 }
