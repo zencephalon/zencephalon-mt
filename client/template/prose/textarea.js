187d215395165d72e68da47eaa7aa471f00f4dfa
Matthew Bunday <zenceph@gmail.com>
2014-11-07 16:40:04 -0500

    Fix deprecation warning.

--- client/template/prose/textarea.js
+++ client/template/prose/textarea.js
@@ -1,14 +1,16 @@
-Template.textarea.settings = function() {
-  return {
-    position: "bottom",
-    limit: 5,
-    rules: [{
-      token: '\\]\\(',
-      replacement: '](',
-      end_token: ')',
-      collection: Proses,
-      field: "url",
-      template: Template.prose_url_title
-    }]
+Template.textarea.helpers({
+  settings: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: '\\]\\(',
+        replacement: '](',
+        end_token: ')',
+        collection: Proses,
+        field: "url",
+        template: Template.prose_url_title
+      }]
+    }
   }
-}
+});
