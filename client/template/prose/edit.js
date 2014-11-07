f36b185564421d715d9d6480da5695d5b447e2c0
Matthew Bunday <zenceph@gmail.com>
2014-11-07 16:42:01 -0500

    Fix deprecation warning.

--- client/template/prose/edit.js
+++ client/template/prose/edit.js
@@ -1,25 +1,25 @@
-Template.prose_edit.displayShortCuts = function() {
-  return !!Session.get("display_shortcuts");
-}
-
-Template.prose_edit.displayFormatting = function() {
-  return !!Session.get("display_formatting");
-}
-
-Template.prose_edit.settings = function() {
-  return {
-    position: "bottom",
-    limit: 5,
-    rules: [{
-      token: '\\]\\(',
-      replacement: '](',
-      end_token: ')',
-      collection: Proses,
-      field: "url",
-      template: Template.prose_url_title
-    }]
-  }
-}
+Template.prose_edit.helpers({
+  displayShortCuts: function() {
+    return !!Session.get("display_shortcuts");
+  },
+  displayFormatting: function() {
+    return !!Session.get("display_formatting");
+  },
+  settings: function() {
+    return {
+      position: "bottom",
+      limit: 5,
+      rules: [{
+        token: '\\]\\(',
+        replacement: '](',
+        end_token: ')',
+        collection: Proses,
+        field: "url",
+        template: Template.prose_url_title
+      }]
+    }
+  } 
+});
 
 Template.prose_edit.rendered = function() {
   $(document).ready(function() {
