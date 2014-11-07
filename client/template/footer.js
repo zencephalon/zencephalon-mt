11e2f04077fbac967e9831cf850416d3a23f497c
Matthew Bunday <zenceph@gmail.com>
2014-11-07 16:37:08 -0500

    Fix deprecated helpers.

--- client/template/footer.js
+++ client/template/footer.js
@@ -1,18 +1,18 @@
-Template.footer.word_count = function() {
-  if (count = Counts.get()) {
-    return count.word_count;
+Template.footer.helpers({
+  word_count: function() {
+    if (count = Counts.get()) {
+      return count.word_count;
+    }
+  },
+  prose_count: function() {
+    if (count = Counts.get()) {
+      return count.prose_count;
+    }
+  },
+  content: function() {
+    template = Prose.get("__footer_template__");
+    if (template._id && template.getBranch()) {
+      return template.getBranch().text;
+    }
   }
-}
-
-Template.footer.prose_count = function() {
-  if (count = Counts.get()) {
-    return count.prose_count;
-  }
-}
-
-Template.footer.content = function() {
-  template = Prose.get("__footer_template__");
-  if (template._id && template.getBranch()) {
-    return template.getBranch().text;
-  }
-}
+});
