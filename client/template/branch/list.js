4af11853cbc210f0535f425c2f2784d68ba5a883
Matthew Bunday <zenceph@gmail.com>
2014-11-07 16:42:40 -0500

    Fix deprecation warning.

--- client/template/branch/list.js
+++ client/template/branch/list.js
@@ -1,35 +1,37 @@
-Template.branch_list.branch_display = function(current_branch, parent_branch_length, prose) {
-  branch_names = _.pluck(_Branches.find({prose: prose._id}).fetch(), 'name');
-  output = "<table><tr>";
-  function branch_url(branch, parent_branch_length) {
-    // FIXME: this is bad coupling.
-    prose = new Prose(prose);
-    return "<a href='/" + prose.url + "/b/" + branch + "'" + (branch === prose.getBranch().name ? "class='active'" : "") + ">" + branch.substr(parent_branch_length) + "</a>";
-  }
-
-  while (_.contains(branch_names, current_branch)) {
-    output += "<td>";
-
-    if (_.contains(branch_names, current_branch + 'a0')) {
-      output += "<table><tr><td>";
-      output += branch_url(current_branch, parent_branch_length);
-      output += "</td></tr>";
-
-      next_lat_code = 'a';
-      while (_.contains(branch_names, current_branch + next_lat_code + '0')) {
-        output += "<tr><td>";
-        output += Template.branch_list.branch_display(current_branch + next_lat_code + '0', current_branch.length + next_lat_code.length, prose);
-        output += "</td></tr>";
-        next_lat_code = Branches.next_lateral_code(next_lat_code);
-      }
-      output += "</table>";
-    } else {
-      output += branch_url(current_branch, parent_branch_length);
+Template.branch_list.helpers({
+  branch_display: function(current_branch, parent_branch_length, prose) {
+    branch_names = _.pluck(_Branches.find({prose: prose._id}).fetch(), 'name');
+    output = "<table><tr>";
+    function branch_url(branch, parent_branch_length) {
+      // FIXME: this is bad coupling.
+      prose = new Prose(prose);
+      return "<a href='/" + prose.url + "/b/" + branch + "'" + (branch === prose.getBranch().name ? "class='active'" : "") + ">" + branch.substr(parent_branch_length) + "</a>";
     }
 
-    output += "</td>";
-    current_branch = Branches.next_sequential_branch(current_branch);
+    while (_.contains(branch_names, current_branch)) {
+      output += "<td>";
+
+      if (_.contains(branch_names, current_branch + 'a0')) {
+        output += "<table><tr><td>";
+        output += branch_url(current_branch, parent_branch_length);
+        output += "</td></tr>";
+
+        next_lat_code = 'a';
+        while (_.contains(branch_names, current_branch + next_lat_code + '0')) {
+          output += "<tr><td>";
+          output += Template.branch_list.branch_display(current_branch + next_lat_code + '0', current_branch.length + next_lat_code.length, prose);
+          output += "</td></tr>";
+          next_lat_code = Branches.next_lateral_code(next_lat_code);
+        }
+        output += "</table>";
+      } else {
+        output += branch_url(current_branch, parent_branch_length);
+      }
+
+      output += "</td>";
+      current_branch = Branches.next_sequential_branch(current_branch);
+    }
+    output += "</tr></table>";
+    return output;
   }
-  output += "</tr></table>";
-  return output;
-}
+});
