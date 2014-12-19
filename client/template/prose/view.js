09f1cbba10352ce79a239cfcf1eb44ed599c6b13
Matthew Bunday <zenceph@gmail.com>
2014-12-19 14:31:28 -0500

    Progress made on saving contenteditable wikis.

--- client/template/prose/view.js
+++ client/template/prose/view.js
@@ -2,6 +2,23 @@ Template.prose_view.events({
   'click h1.title': function() {
     View.setViewMode(this.prose.url, false);
   },
+  'keyup': function(e) {
+    $(event.target).change();
+  },
+  'change': function(e) {
+    var $t = $(e.target);
+    console.log($t.html());
+    // var prose = Prose.id($t.data('id'));
+    // console.log(prose);
+    console.log($t.contents());
+    var c = $t.contents().filter(function() {
+      return !$(this).hasClass('subview');
+    });
+    console.log(c);
+    c = $.map(c, function(node) {return (node.nodeType == 3) ? node.data : node.outerHTML}).join("");
+    console.log(c);
+    this.prose.save({url: this.prose.url, text: c, title: this.prose.title}, this.branch, false);
+  },
   'mouseover a': function(e) {
     var target = $(e.target);
     var href = target.attr('href');
